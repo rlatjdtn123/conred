b5ac2e5ac610738cbb67ee3a5cfd23f48911ee39
syhnz <syhnz@121.135.192.237>
2020-05-30 17:25:28 +0900

    junit 임포트줄 제거 import org.junit.internal.matchers.SubstringMatcher;

--- conred_final/src/main/java/com/hk/conred/Sungsu.java
+++ conred_final/src/main/java/com/hk/conred/Sungsu.java
@@ -9,7 +9,6 @@ import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.junit.internal.matchers.SubstringMatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
